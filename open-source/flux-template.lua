94ed17ef1e88b1be09cdd855e1874ee24ca81ec9
Stebulous <sky34416@gmail.com>
2021-03-03 13:12:06 -0500

    Create flux-template.lua

---
+++ open-source/flux-template.lua
@@ -0,0 +1,44 @@
+--[[
+
+Flux Lib Template
+by Dosage#9999
+
+]]--
+
+--init
+local Flux = loadstring(game.HttpGet('https://raw.githubusercontent.com/Simak90/pfsetcetc/main/fluxsex.lua')())
+local cred = "Made by Dosage#9999"
+
+local name = "Game GUI"
+
+local win = Flux:Window(name, cred, Color3.fromRGB(255, 110, 48), Enum.KeyCode.RightShift)
+print(cred)
+local player = game:GetService("Players").LocalPlayer
+
+-- kick if wrong game
+if not game.PlaceId == 1234 then
+    player.Kick("Wrong Game")
+end
+
+
+--tab 1
+local tab1 = win:Tab("Main", "http://www.roblox.com/asset/?id=6023426915")
+
+tab:Slider("Walkspeed", "Makes your faster.", 0, 100,16,function(t)
+    while wait() do
+        if t then
+            player.Humanoid.Walkspeed = t
+        end
+    end
+end)
+
+
+
+--tab 2
+local tab2 = win:Tab("Options", "http://www.roblox.com/asset/?id=6022668888")
+
+tab2:Button("Kill GUI", "Destroys the GUI", function()
+    game.CoreGui[name]:Destroy()
+end)
+
+tab2:Label("Toggle the GUI with RShift.")
