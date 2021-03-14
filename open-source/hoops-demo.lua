5afdf9f805ad4ca8d4def7af7f9847a0aff23ba2
Stebulous <sky34416@gmail.com>
2021-03-13 22:07:19 -0500

    Update hoops-demo.lua

--- open-source/hoops-demo.lua
+++ open-source/hoops-demo.lua
@@ -17,10 +17,9 @@ end
 local page = venyx:addPage("Main", 5012544693)
 local section1 = page:addSection("Aimbot")
 local section2 = page:addSection("Misc")
-local debounce = {
-    state = false,
-    time = 1
-}
+local debounce = {}
+debounce.state = false
+debunce.time = 1
 
 section1:addKeybind("Aimbot Keybind", Enum.KeyCode.R, function()
 if debounce.state == false then
@@ -35,18 +34,36 @@ end, function()
 print("Changed Keybind")
 end)
 
-section1:addToggle("Arm Stretch", nil, function(value)
-    getgenv().arms = value
+section1:addToggle("Toggle Dunk Strength", nil, function(value)
     while wait() do
-        if getgenv().arms == true then
-            player.Character["Left Arm"].Size = Vector3.new(16, 2, 1)
-            player.Character["Right Arm"].Size = Vector3.new(16, 2, 1)
-            else
-            player.Character["Left Arm"].Size = Vector3.new(1, 2, 1)
-            player.Character["Right Arm"].Size = Vector3.new(1, 2, 1)
+        wait()
+        local strength = 1e7 --put what strength you want here
+        local Plr = game:GetService("Players").LocalPlayer
+        local descs = Plr.Character:GetDescendants()
+        local desc = descs[i]
+
+        getgenv().toggle = value
+    
+            if Plr.Character and getgenv().toggle == true then
+                for i = 1, #descs do
+                if desc:IsA"BasePart" then
+                    desc.CustomPhysicalProperties = PhysicalProperties.new(100, 0.3, 0.5)
+                end
+            end
+        end
+      end
+      
+      if getgenv().toggle == false or nil then
+        if Plr.Character then
+            for i = 1, #descs do
+            if desc:IsA"BasePart"then
+                desc.CustomPhysicalProperties = PhysicalProperties.new(0.699999988, 0.300000012, 0, 1, 100)
+            end
         end
     end
-end)
+  end
+      end
+    end)
 
 section2:addButton("Kill GUI", function()
 game.CoreGui["Made by Dosage#9999"]:Destroy()
